c3ab2ff7941b6724d8f752ac1686ac75b67d583c
Ezesky14 <kouame14@gmail.com>
2019-10-05 12:49:39 +0000

    Premier ajout de l'application

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,17 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  //let fixture: ComponentFixture<MypopupComponent>;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        ReactiveFormsModule
       ],
       declarations: [
         AppComponent
@@ -20,16 +25,84 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-gouminator'`, () => {
+  // it(`should have as title 'angular-gouminator'`, () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   const app = fixture.debugElement.componentInstance;
+  //   expect(app.title).toEqual('angular-gouminator');
+  // });
+
+  // it('should render title', () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   fixture.detectChanges();
+  //   const compiled = fixture.debugElement.nativeElement;
+  //   expect(compiled.querySelector('.content span').textContent).toContain('angular-gouminator app is running!');
+  // });
+
+
+  it(`Verifie que le calcul du volume fonctionne bien`, () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('angular-gouminator');
+    const app = fixture.componentInstance;
+    let calculerVolumeSphere = app.calculerVolumeSphere(33);
+    expect(calculerVolumeSphere).toBeTruthy();
   });
 
-  it('should render title', () => {
+
+
+  it(`Verifie que la validation est impossible lorsque le champ rayon est vide : `, () => {
     const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-gouminator app is running!');
+    const app = fixture.componentInstance;
+    app.formGroup.patchValue({rayon:undefined});
+    expect(app.formGroup.invalid).toBe(true);    
+  });
+
+  it(`Verifie que la validation est impossible lorsque le rayon est superieur à 100 : `, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    app.formGroup.patchValue({rayon:1000});
+    expect(app.formGroup.invalid).toBe(true);
+    
+  });
+
+  it(`Verifie que la validation est impossible lorsque le rayon entré est une chaine decaractère : `, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    app.formGroup.patchValue({rayon:'100dd'});
+    expect(app.formGroup.invalid).toBe(true);
+    
+  });
+
+
+
+  it(`Verifie que le resultat est affiché lorsque la validation est bonne `, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    app.formGroup.patchValue({rayon:54});
+    app.submit();
+    expect(app.result).toBeTruthy();
+    
+  });
+
+
+  it(`Verifie que le cache ajoute les informations de calculs lorsque la validation est bonne  `, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    app.formGroup.patchValue({rayon:10});
+    app.submit();
+    expect(app.historique.length).toBeGreaterThan(0);
+  });
+
+
+
+  it(`Verifie que si l'utilisateur saisit le même rayon, la réponse est renvoyée d'un cache au lieu de la calculer `, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    app.formGroup.patchValue({rayon:10});
+    app.submit();
+
+    const resToVerify= app.result;
+    app.formGroup.patchValue({rayon:15});
+    app.submit();
+    app.formGroup.patchValue({rayon:10});
+    expect(app.result).toEqual(resToVerify);
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-gouminator';
+  formGroup: FormGroup;
+  result;
+  historique = [];
+  dejaCalculer = false;
+
+  ngOnInit() {
+  
+
+  }
+
+  constructor(private formBuilder: FormBuilder) {
+    this.formGroup = this.formBuilder.group({
+      rayon: ['', [Validators.required, Validators.max(100),Validators.pattern('^[0-9_.+-]+$')]]
+    })
+
+
+    this.formGroup.get('rayon').valueChanges.subscribe(val => {
+      this.dejaCalculer = false;
+      const res = this.verifierDansHistorique(val);
+      if (res.exist) {
+        this.dejaCalculer = true;
+        this.result = this.historique[res.position].resultat;
+      }
+    })
+
+
+  }
+
+
+
+  ajouterDansHistorique(rayon: number, resultat) {
+    this.historique.push({ rayon: rayon, resultat: resultat });
+  }
+
+  verifierDansHistorique(rayonEntree) {
+    if (this.historique.length === 0) {
+      return { exist: false, position: -1 };
+    } else {
+      const posHistorique = this.historique.map(el => el.rayon).indexOf(rayonEntree);
+      let existDansHistorique = (posHistorique !== -1);
+      return { exist: existDansHistorique, position: posHistorique };
+    }
+
+  }
+  submit() {
+    if (!this.formGroup.invalid) {
+      const r = this.formGroup.value.rayon;
+      this.result = this.calculerVolumeSphere(r);
+      this.ajouterDansHistorique(r, this.result);
+    }
+
+  }
+  calculerVolumeSphere(rayon: number) {
+    return 4 / 3 * Math.PI * Math.pow(rayon, 3);
+  }
+
+
 }
